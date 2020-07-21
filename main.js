625bf94657b808d7bcd86961eea8d995d38ec9e8
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-21 18:39:29 +0800

    pass the test

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 function createMultiplyTable(start, end) {
+  if(isValid(start, end)){
+    return PrintMultiplyTable(start, end);
+  }
   return null;
+}
+
+function isValid(start, end){
+  return IsEndBiggerThanStart(start, end) && IsStsrtAndEndInRange(start, end);
+}
+
+function IsEndBiggerThanStart(start, end){
+  if(start > end) return false;
+  return true;
+}
+
+function IsStsrtAndEndInRange(start, end){
+  if(start >= 1 && start <= 1000 && end >= 1 && end <= 1000) return true;
+  return false;
+}
+
+function PrintMultiplyTable(start, end){
+  var MultiplyTable = '';
+  for(var i = start;i <= end;i++){
+    for(var j = start; j <= i;j++){
+      MultiplyTable += j+'*'+i+'='+i*j+'\t';
+    }
+    MultiplyTable = MultiplyTable.substring(0,MultiplyTable.length-1);
+    if(i<end) MultiplyTable +='\n';
+  }
+  return MultiplyTable;
 }
 
 module.exports = {
